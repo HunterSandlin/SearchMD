fdcf49a70ac768ec8d28dfe2985b053b8ffd227a
MariannSF <mariann.freund@gmail.com>
2022-04-10 22:59:46 -0400

    <edited SQL file user table>

--- retiredSearchMD.sql
+++ retiredSearchMD.sql
@@ -23,22 +23,21 @@ DROP TABLE IF EXISTS admintable;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE admintable (
-  adminId int NOT NULL AUTO_INCREMENT,
-  userId int DEFAULT NULL,
+  adminId int NOT NULL,
   alName varchar(500) DEFAULT NULL,
   afName varchar(500) DEFAULT NULL,
   userRole varchar(1) DEFAULT 'a',
   PRIMARY KEY (adminId)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `admintable`
 --
 
-INSERT INTO admintable (adminId, userId, alName, afName, userRole) VALUES (2,6,'Admin','Bob','a');
-INSERT INTO admintable (adminId, userId, alName, afName, userRole) VALUES (8,7,'Admin2','Anna','a');
-INSERT INTO admintable (adminId, userId, alName, afName, userRole) VALUES (10,8,'Admin3','John','a');
+INSERT INTO admintable (adminId, alName, afName, userRole) VALUES (2,'Admin','Bob','a');
+INSERT INTO admintable (adminId, alName, afName, userRole) VALUES (8,'Admin2','Anna','a');
+INSERT INTO admintable (adminId, alName, afName, userRole) VALUES (10,'Admin3','John','a');
 
 --
 -- Table structure for table `appointmenttable`
@@ -64,6 +63,7 @@ CREATE TABLE appointmenttable (
 -- Dumping data for table `appointmenttable`
 --
 
+INSERT INTO appointmenttable (appId, patientId, doctorId, appDate) VALUES (44,2,8,'2022-01-13 00:00:00');
 
 --
 -- Table structure for table `doctortable`
@@ -74,7 +74,6 @@ DROP TABLE IF EXISTS doctortable;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE doctortable (
   doctorId int NOT NULL,
-  userId int DEFAULT NULL,
   dlName varchar(500) DEFAULT NULL,
   dfName varchar(500) DEFAULT NULL,
   specialty varchar(500) DEFAULT NULL,
@@ -87,9 +86,9 @@ CREATE TABLE doctortable (
 -- Dumping data for table `doctortable`
 --
 
-INSERT INTO doctortable (doctorId, userId, dlName, dfName, specialty, userRole) VALUES (4,3,'Smith','Bob','genereal','d');
-INSERT INTO doctortable (doctorId, userId, dlName, dfName, specialty, userRole) VALUES (8,4,'Boyer','Anna','brain','d');
-INSERT INTO doctortable (doctorId, userId, dlName, dfName, specialty, userRole) VALUES (10,5,'Monroe','John','genereal','d');
+INSERT INTO doctortable (doctorId, dlName, dfName, specialty, userRole) VALUES (4,'Smith','Bob','genereal','d');
+INSERT INTO doctortable (doctorId, dlName, dfName, specialty, userRole) VALUES (8,'Boyer','Anna','brain','d');
+INSERT INTO doctortable (doctorId, dlName, dfName, specialty, userRole) VALUES (10,'Monroe','John','genereal','d');
 
 --
 -- Table structure for table `patienttable`
@@ -100,7 +99,6 @@ DROP TABLE IF EXISTS patienttable;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE patienttable (
   patientId int NOT NULL,
-  userId int DEFAULT NULL,
   plName varchar(500) DEFAULT NULL,
   pfName varchar(500) DEFAULT NULL,
   dob datetime DEFAULT NULL,
@@ -114,8 +112,10 @@ CREATE TABLE patienttable (
 -- Dumping data for table `patienttable`
 --
 
-INSERT INTO patienttable (patientId, userId, plName, pfName, dob, insuranceId, userRole) VALUES (1,2,'Taylor','Ellis','2019-06-19 00:00:00','898080','p');
-INSERT INTO patienttable (patientId, userId, plName, pfName, dob, insuranceId, userRole) VALUES (2,1,'Meyers','Adele','2019-06-19 00:00:00','898080','p');
+INSERT INTO patienttable (patientId, plName, pfName, dob, insuranceId, userRole) VALUES (1,'Taylor','Ellis','2019-06-19 00:00:00','898080','p');
+INSERT INTO patienttable (patientId, plName, pfName, dob, insuranceId, userRole) VALUES (2,'Meyers','Adele','2019-06-19 00:00:00','898080','p');
+INSERT INTO patienttable (patientId, plName, pfName, dob, insuranceId, userRole) VALUES (90,'Taylor','Ellis','2019-06-19 00:00:00','0898797','p');
+INSERT INTO patienttable (patientId, plName, pfName, dob, insuranceId, userRole) VALUES (99,'Evian','Ellis','2019-06-19 00:00:00','0898797','p');
 
 --
 -- Table structure for table `usertable`
@@ -127,22 +127,26 @@ DROP TABLE IF EXISTS usertable;
 CREATE TABLE usertable (
   userId int NOT NULL AUTO_INCREMENT,
   `password` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (userId)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  patientId int DEFAULT NULL,
+  doctorId int DEFAULT NULL,
+  adminId int DEFAULT NULL,
+  PRIMARY KEY (userId),
+  KEY patientId (patientId),
+  KEY doctorId (doctorId),
+  KEY adminId (adminId),
+  CONSTRAINT usertable_ibfk_1 FOREIGN KEY (patientId) REFERENCES patienttable (patientId) ON DELETE CASCADE,
+  CONSTRAINT usertable_ibfk_2 FOREIGN KEY (doctorId) REFERENCES doctortable (doctorId) ON DELETE CASCADE,
+  CONSTRAINT usertable_ibfk_3 FOREIGN KEY (adminId) REFERENCES admintable (adminId) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usertable`
 --
 
-INSERT INTO usertable (userId, password) VALUES (1,'User1');
-INSERT INTO usertable (userId, password) VALUES (2,'User2');
-INSERT INTO usertable (userId, password) VALUES (3,'User3');
-INSERT INTO usertable (userId, password) VALUES (4,'User1');
-INSERT INTO usertable (userId, password) VALUES (5,'User2');
-INSERT INTO usertable (userId, password) VALUES (6,'User3');
-INSERT INTO usertable (userId, password) VALUES (7,'User3');
-INSERT INTO usertable (userId, password) VALUES (8,'User3');
+INSERT INTO usertable (userId, password, patientId, doctorId, adminId) VALUES (11,'User1',1,4,2);
+INSERT INTO usertable (userId, password, patientId, doctorId, adminId) VALUES (21,'User2',1,8,2);
+INSERT INTO usertable (userId, password, patientId, doctorId, adminId) VALUES (31,'User3',1,4,10);
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -153,4 +157,4 @@ INSERT INTO usertable (userId, password) VALUES (8,'User3');
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-10 22:57:25
+-- Dump completed on 2022-04-10 11:23:00
