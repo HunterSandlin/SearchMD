154a29cdd4a92580dee30511294d8c536ddd3d58
Hunter Sandlin <huntergsandlin@gmail.com>
2022-04-12 18:38:14 -0400

    Set App to load homepage by default

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchmdComponent from './components/SearchmdComponent';
 import NavbarComponent from './components/NavbarComponent';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
-      <SearchmdComponent />
+      <HomePage />
     </div>
   );
 }
