1e341bd0d41befd7128e76f3dd9facdef42ef355
Varun Suresh <vasur2201@gmail.com>
2022-04-14 13:40:22 -0400

    Added add appointment functionality

---
+++ src/main/java/com/searchmd/searchmd/models/AppointmenttableRepository.java
@@ -0,0 +1,7 @@
+package com.searchmd.searchmd.models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppointmenttableRepository extends CrudRepository<Appointmenttable,Integer> {
+    
+}
