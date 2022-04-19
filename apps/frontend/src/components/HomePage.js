fb05a7701811b662ea3ad003c7553dd4fedd4f9c
Hunter Sandlin <huntergsandlin@gmail.com>
2022-04-19 19:17:32 -0400

    Added searchbar and changed link names

--- apps/frontend/src/components/HomePage.js
+++ apps/frontend/src/components/HomePage.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import {Button, Row, Col, Container} from 'react-bootstrap';
+import LoginModal from './LoginModal';
 
 class HomePage extends React.Component{
     constructor(props){
         super(props)
-        this.state ={}
+        this.state = { 
+            isOpen: false 
+        };
     }
     componentDidMount(){
     }
+
+    toggleModal = () => {
+        console.log("Toggled");
+        console.log(this.state.isOpen);
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+      }
+
     render(){
+           const backdropStyle = {
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'rgba(0,0,0,0.3)',
+      padding: 50
+    };
+
+    const modalStyle = {
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      maxWidth: 500,
+      minHeight: 300,
+      margin: '0 auto',
+      padding: 30
+    };
         const divStyle = {
             marginLeft: '100px',
             maginTop: '500px',
@@ -19,33 +49,48 @@ class HomePage extends React.Component{
             width: '180px',
             height: '70px'
           };
+
+
         return(
-        <Container>
-          <Row className="d-flex py-5 align-items-center align-middle align-items-center">
-            <Col className="col col-lg-12 col-md-12 text-center fixed align-middle align-items-center">
-                <h1 className='mb-3'>Welcome to SearchMD</h1>
-            </Col>
-          </Row>
-          <Row className="d-flex py-5 align-items-center align-middle align-items-center">
-            <Col className="col col-lg-12 col-md-12 text-center fixed align-middle align-items-center">
-            <div className=' justify-content-center'>
-                    <Row style={{marginBottom:"50px"}}>
-                        <h4 className='mb-3 text-secondary'>Continue As</h4>
-                    </Row>
-                    <Row>
-                        <div>
-                            <Button style={divStyle} bsStyle="primary" bsSize="large" className="center-btn">
-                                Patient
-                            </Button>
-                            <Button style={divStyle} bsStyle="primary" bsSize="large" className="center-btn">
-                                Doctor
-                            </Button>
+            <div>
+                <Container>
+                <Row className="d-flex py-5 align-items-center align-middle align-items-center">
+                    <Col className="col col-lg-12 col-md-12 text-center fixed align-middle align-items-center">
+                        <h1 className='mb-3'>Welcome to SearchMD</h1>
+                    </Col>
+                </Row>
+                <Row className="d-flex py-5 align-items-center align-middle align-items-center">
+                    <Col className="col col-lg-12 col-md-12 text-center fixed align-middle align-items-center">
+                    <div className=' justify-content-center'>
+                            <Row style={{marginBottom:"50px"}}>
+                                <h4 className='mb-3 text-secondary'>Continue As</h4>
+                            </Row>
+                            <Row>
+                                <div>
+                                    <Button onClick={this.toggleModal} style={divStyle} className="center-btn">
+                                        Patient
+                                    </Button>
+                                    <Button onClick={this.toggleModal} style={divStyle} className="center-btn">
+                                        Doctor
+                                    </Button>
+                                </div>
+                            </Row>
                         </div>
-                    </Row>
+                    </Col>
+                </Row>
+                </Container>
+
+                <LoginModal onClose={this.toggleModal} show={true} />
+
+                {
+                this.state.isOpen &&
+                <div>
+                    Login Modal
                 </div>
-            </Col>
-          </Row>
-        </Container>
+                }
+
+
+            </div>
         )
     }
 }
--- apps/frontend/src/components/NavbarComponent.js
+++ apps/frontend/src/components/NavbarComponent.js
@@ -1,4 +1,4 @@
-import {Navbar,Container, Nav} from 'react-bootstrap'
+import {Navbar,Container, Nav, Form, Button, FormControl} from 'react-bootstrap'
 import React from 'react'
 
 class NavbarComponent extends React.Component{
@@ -16,9 +16,19 @@ class NavbarComponent extends React.Component{
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="#">Home</Nav.Link>
+                  <Nav.Link href="#">Appointment</Nav.Link>
+                  <Nav.Link href="#">Doctors</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
+              <Form className="d-flex">
+                  <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-primary">Search</Button>
+                </Form>
             </Container>
           </Navbar>
         )
