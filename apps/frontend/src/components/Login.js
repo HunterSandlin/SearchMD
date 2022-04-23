b398b170c596b07d6837db4dd5b9d5041fdee5e1
Hunter Sandlin <huntergsandlin@gmail.com>
2022-04-23 13:13:53 -0400

    Replaced loginModal with Login

---
+++ apps/frontend/src/components/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Button, Row, Col, Container, Form, Card} from 'react-bootstrap';
+
+
+class Login extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = { 
+        };
+    }
+
+    componentDidMount(){
+    }
+
+
+    render(){
+          const paddingStyle = {
+            padding: 10
+          };
+
+        return(
+            <Container className="d-flex">
+                <Row className="m-auto align-self-center">
+                    <Col  style={{padding:'3rem'}}>
+                        <Card style={{width:'40rem'}}>
+                            <Card.Header>Login as {this.props.type}</Card.Header>
+                            <Card.Body>
+                                <Form>
+                                <Form.Group className="mb-3" style={paddingStyle} controlId="formBasicEmail">
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" style={paddingStyle} controlId="formBasicPassword">
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>               
+        )
+    }
+}
+export default Login;
