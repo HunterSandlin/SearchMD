9318a35c01f6355b0bfc419e6821639e9173c196
Hunter Sandlin <huntergsandlin@gmail.com>
2022-04-25 12:02:10 -0400

    Changed styling on cards

--- apps/frontend/src/components/Appointments.js
+++ apps/frontend/src/components/Appointments.js
@@ -15,42 +15,26 @@ class Appointments extends React.Component{
 
     render(){
 
-        const divStyle = {
-            marginLeft: '50px',
-            maginTop: '250px',
-            marginRight: '50px',
-            marginBotton: 'auto',
-            borderRadius: '1rem',
-            width: '20rem',
-            height: 'auto', 
-            align: 'flex'
+        const divPadding = {
+            padding: '20px'
         };
-
-
         return(
-            <div>
-                <Container>
-                <Row className="d-flex py-5 align-items-center align-middle align-items-center">
-                    <Col className="col col-lg-12 col-md-12 text-center fixed align-middle align-items-center">
-                        <h3 className='mb-3'>Your appointments</h3>
-                    </Col>
-                </Row>
-                <Row className="d-flex py-2 align-items-center align-middle align-items-center">
-                    <Col className="col col-lg-12 col-md-12 text-center fixed align-middle align-items-center">
-                        <div style={divStyle} className= ' justify-content-center'>
+            <Container className="d-flex" style={divPadding}>
+                <Row className="m-auto align-self-center">
+                    <Col>
                         <AppointmentsCard doctorName="Dr. Phil" aptDate="4/20/2022 at 11:00 AM" />
                         <AppointmentsCard doctorName="Dr. Myers" aptDate="4/15/2022 at 9:30 AM"/>
                         <AppointmentsCard doctorName="Dr. Oz" aptDate="4/11/2022 at 1:45 PM"/>
                         <AppointmentsCard doctorName="Josh" aptDate="4/5/2022 at 2:45 PM" />
-                        </div>
+                    </Col>
+                    <Col>
+                        <AppointmentsCard doctorName="Dr. Jason" aptDate="5/10/2022 at 9:00 AM" />
+                        <AppointmentsCard doctorName="Dr. Myers" aptDate="6/25/2022 at 10:45 AM"/>
+                        <AppointmentsCard doctorName="Dr. Azimov" aptDate="6/23/2022 at 8:00 PM"/>
+                        <AppointmentsCard doctorName="Josh" aptDate="6/26/2022 at 5:00 PM" />
                     </Col>
                 </Row>
-                
-                </Container>
-                
-                
-
-            </div>
+            </Container>  
         )
     }
 }
--- apps/frontend/src/components/AppointmentsCard.js
+++ apps/frontend/src/components/AppointmentsCard.js
@@ -20,65 +20,38 @@ class AppointmentsCard extends React.Component{
             marginRight: '50px',
             marginBotton: 'auto',
             borderRadius: '1rem',
-            width: '20rem',
+            width: '30rem',
             height: 'auto'
         };
 
-        const buttonConfirmStyle = {
-            marginLeft: '25px',
-            maginTop: '250px',
-            marginRight: '25px',
-            marginBotton: 'auto',
-            borderRadius: '1rem',
-            magin: '50px',
-            width: 'auto',
-            height: 'auto',
-            backgroundColor: 'rgb(40, 150, 20)'
+        const divPadding = {
+            padding: '10px'
         };
 
-        const buttonCancelStyle = {
-            marginLeft: '25px',
-            maginTop: '250px',
-            marginRight: '25px',
-            marginBotton: 'auto',
-            borderRadius: '1rem',
-            magin: '50px',
-            width: 'auto',
-            height: 'auto',
-            backgroundColor: 'rgb(170, 10, 10)'
-        };
-
-
-        // `onClick` should be updated
         return(
-            <div>
-                <Row className="d-flex py-5 align-items-center align-middle align-items-center">
-                    <Card style={cardStyle}>
-                        <Card.Body>
-                            <Card.Title>Upcoming Appointment</Card.Title>
-                            <Card.Text>
-                                You have an appointment at the Health Center with {this.props.doctorName} 
-                                , on {this.props.aptDate}. 
-                                Please complete any necessary pre-visit forms by logging in to your patient <Card.Link href="#">health portal</Card.Link>. 
-                            </Card.Text>
-                                <Row>
-                                    <div>
-                                        <Button onClick={this.toggleModal} style={buttonConfirmStyle}  className="center-btn">
+            <div style={divPadding}>
+                <Card style={cardStyle}>
+                    <Card.Body>
+                        <Card.Title>Upcoming Appointment</Card.Title>
+                        <Card.Text>
+                            You have an appointment at the Health Center with {this.props.doctorName} 
+                            , on {this.props.aptDate}. 
+                            Please complete any necessary pre-visit forms by logging in to your patient <Card.Link href="#">health portal</Card.Link>. 
+                        </Card.Text>
+                            <Row>
+                                <Col> 
+                                    <Button>
                                         Confirm
-                                        </Button>
-                                        <Button onClick={this.toggleModal} style={buttonCancelStyle} className="center-btn">
+                                    </Button>
+                                </Col>
+                                <Col> 
+                                    <Button className="btn-danger">
                                         Cancel
-                                        </Button>
-                                    </div>
-                                </Row>
-                        </Card.Body>
-                    </Card> 
-                </Row>
-
-
-
-
-                
+                                    </Button>
+                                </Col>
+                            </Row>
+                    </Card.Body>
+                </Card> 
             </div>
         )
     }
